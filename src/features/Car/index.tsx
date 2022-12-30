df57d25cb6879ca75e005d2da5e59ec8568d4f87
Anh Cao <anh.cao@perchinsights.com>
2022-12-30 15:32:03 -0500

    Added CarPhoto with random picture API to display fake car photo

--- src/features/Car/index.tsx
+++ src/features/Car/index.tsx
@@ -4,6 +4,9 @@ import {
   Text,
   Button,
   TouchableOpacity,
+  View,
+  Image,
+  StyleSheet,
 } from 'react-native';
 import {ICar} from '../../hooks/useCars';
 
@@ -11,10 +14,45 @@ export interface ICarProps<DataType> {
   data: DataType;
   onPress: (event: GestureResponderEvent) => void;
 }
+export interface ICarPhotoProps {
+  uri: string;
+  styles: {
+    container: {
+      paddingTop?: number;
+      width?: number;
+      height?: number;
+    };
+    photo: {
+      width: number;
+      height: number;
+    };
+  };
+}
+const carStyles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  photo: {
+    width: 300,
+    height: 200,
+  },
+});
+
+const CarPhoto = ({uri, styles}: ICarPhotoProps) => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.photo} source={{uri: uri}} />
+    </View>
+  );
+};
 const Car = ({data, onPress}: ICarProps<ICar>) => {
   const {make, model, year, price} = data;
   return (
     <TouchableOpacity onPress={onPress}>
+      <CarPhoto
+        uri={`https://picsum.photos/seed/picsum/${carStyles.photo.width}/${carStyles.photo.height}`}
+        styles={carStyles}
+      />
       <Text>
         {year} {make} {model}
       </Text>
