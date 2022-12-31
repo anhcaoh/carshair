c4f26b2ee01f7390a42a28d2850b09093774bc69
Anh Cao <anh.cao@perchinsights.com>
2022-12-31 00:45:55 -0500

    Reuse Car for CarDetails

--- src/features/CarDetails/index.tsx
+++ src/features/CarDetails/index.tsx
@@ -12,6 +12,7 @@ import {useRoute} from '@react-navigation/native';
 import {RootStackParamList} from '../../../App';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import useCar from '../../hooks/useCar';
+import Car from '../Car';
 export interface ICarDetailsProps {
   id: string;
 }
@@ -86,51 +87,28 @@ const CarDetails = (_props: CarDetailsProps) => {
   const {
     params: {id, vin},
   } = useRoute<CarDetailsProps['route']>();
-  const {car: make, car_model: model, car_model_year: year} = useCar({id, vin});
+  const {
+    car: make,
+    car_model: model,
+    car_model_year: year,
+    car_color: color,
+    price,
+  } = useCar({id, vin});
   return (
-    <View style={carStyles.container}>
-      <TouchableOpacity>
-        <View style={carStyles.photoView}>
-          <CarPhoto
-            uri={`https://picsum.photos/${carStyles.photo.width}/${carStyles.photo.height}?ramdon=${id}`}
-            styles={carStyles.photo}
-          />
-        </View>
-        <View style={carStyles.details}>
-          <View>
-            <View style={carStyles.yearMakeModelView}>
-              <Text>
-                #{id} - VIN: {vin}
-              </Text>
-              <Text style={carStyles.text}>
-                {year} {make} {model}
-              </Text>
-            </View>
-            <View style={carStyles.flexRow}>
-              <View style={carStyles.priceView}>
-                <Text>
-                  {/* <Text style={carStyles.price}>{price}</Text> */}
-                  <Text style={carStyles.text}> / day</Text>
-                </Text>
-              </View>
-              <View style={carStyles.learnMoreView}>
-                <Button
-                  title="LEARN MORE"
-                  onPress={() => console.log('LEARN MORE PRESSED')}
-                />
-              </View>
-            </View>
-          </View>
-        </View>
-        <View style={carStyles.bookNowView}>
-          <Button
-            onPress={() => console.log('BOOK NOW PRESSED')}
-            color={carStyles.button.color}
-            title="BOOK NOW"
-          />
-        </View>
-      </TouchableOpacity>
-    </View>
+    <React.Fragment>
+      <Car
+        data={{
+          id,
+          vin,
+          make,
+          model,
+          color,
+          year,
+          price,
+        }}
+        onPress={() => {}}
+      />
+    </React.Fragment>
   );
 };
 export default CarDetails;
